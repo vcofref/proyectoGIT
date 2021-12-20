cf51fc56003fea42f0affa1d53181fab66a0c324
Victor Cofre <victor.cofre.f@gmail.com>
2021-12-19 23:11:46 -0300

    cambio nueva rama

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,9 @@ use Illuminate\Routing\Controller as BaseController;
 
 // prueba de pull
 
+//cambio en nueva rama
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
